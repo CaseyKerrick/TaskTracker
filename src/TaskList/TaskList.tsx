d4d1d29f088a811dc8237257ea921d854faa9dfc
CaseyKerrick <casey.rainn.kerrick@gmail.com>
2023-07-13 14:14:58 -0500

    Adjust task list styling, add days left column.

--- src/TaskList/TaskList.tsx
+++ src/TaskList/TaskList.tsx
@@ -1,18 +1,103 @@
 import React from 'react';
-import { List, ListItem } from '@mui/material';
+import { TableContainer, Table, TableHead, TableBody, TableRow, TableCell } from '@mui/material';
 import { defaultTaskList, Task } from '../tasks';
 import './TaskList.css';
 
 
 function TaskList() {
 
+  type Align = "left" | "right" | "center" | "inherit" | "justify" | undefined;
+  type Column = {
+    label: string,
+    width: string,
+    align: Align,
+  };
+
+  const columns: Column[] = [
+    { label: 'Name', width: '400px', align: 'left' },
+    { label: 'Frequency', width: '100px', align: 'right' },
+    { label: 'Last Performed', width: '200px', align: 'right' },
+    { label: 'Days Left', width: '100px', align: 'center' }
+  ];
+
+  const calculateDaysRemaining = (task: Task) => {
+    if (!task.dateLastPerformed) {
+      return 0;
+    }
+
+    const MS_IN_ONE_DAY = 1000 * 60 * 60 * 24;
+    const today = new Date().getTime();
+
+    const dateLastPerformed = new Date(task.dateLastPerformed).getTime();
+    const dayDifference = Math.floor(Math.abs((dateLastPerformed - today) / MS_IN_ONE_DAY));
+
+    return task.frequency - dayDifference;
+  };
+
+  const colorSelector = (daysRemaining: number, ratio: number, frequency: number): string => {
+    let colorScale = [
+      '#800080', // purple
+      '#fe2626', // red
+      '#ff4500', // orange red
+      'rgb(255, 165, 0)', // orange
+      'rgb(154, 204, 50', // yellow green
+      'rgb(92, 172, 92)', // light green
+      'rgb(4, 124, 4)', //green
+      'rgb(4, 124, 4)', //green
+      'rgb(4, 124, 4)', //green
+      'rgb(4, 124, 4)', //green
+    ];
+
+    if (daysRemaining < 0) {
+      return colorScale[0];
+    } else if (daysRemaining >= 7) {
+      return colorScale[9];
+    } else if (frequency < 7) {
+      return colorScale[ratio];
+    } else {
+      return colorScale[daysRemaining];
+    }
+  };
+
+  const displayDaysRemaining = (daysRemaining: number, frequency: number) => {
+    const ratio = Math.floor(daysRemaining / frequency * 10);
+    const backgroundColor = colorSelector(daysRemaining, ratio, frequency);
+    
+    return (<div className='daysRemainingCell' style={{ backgroundColor }}>{daysRemaining > 0 ? daysRemaining : '0'}</div>);
+  };
+
   return (
     <div className='mainList'>
-      <List>
-        { defaultTaskList.map((task: Task) => (
-          <ListItem key={task.description} className='taskItem'>{task.description}</ListItem>
-        ))}
-      </List>
+      <br />
+      <TableContainer className='taskListContainer'>
+        <Table>
+          <TableHead>
+            <TableRow>
+              {columns.map((column) => (
+                <TableCell
+                  className='taskListHeader'
+                  key={column.label}
+                  style={{ width: column.width }}
+                  align={column.align}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            { defaultTaskList.map(row => (
+              <TableRow key={row.description}>
+                <TableCell>{row.description}</TableCell>
+                <TableCell align='right'>{row.frequency}</TableCell>
+                <TableCell align='right'>{row.dateLastPerformed || 'Never'}</TableCell>
+                <TableCell align='center'>{displayDaysRemaining(calculateDaysRemaining(row), row.frequency)}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <br />
     </div>
   );
 }
--- src/tasks.ts
+++ src/tasks.ts
@@ -1,4 +1,6 @@
 const SIX_MONTHS = 180;
+const ONE_WEEK = 7;
+const TWO_WEEKS = 14;
 
 export type Task = {
   description: string;
@@ -8,13 +10,13 @@ export type Task = {
 };
 
 export const defaultTaskList: Task[] = [
-  { description: 'Wash Dishes', frequency: 1 },
-  { description: 'Scoop Litterbox', frequency: 2 },
-  { description: 'Vacuum Car', frequency: SIX_MONTHS },
-  { description: 'Vacuum Carpet', frequency: 7 },
-  { description: 'Sweep Floors', frequency: 7 },
-  { description: 'Water Plants', frequency: 3 },
-  { description: 'Clip Cat Claws', frequency: 14 },
-  { description: 'Give Stormy Inhaler', frequency: 1 },
-  { description: 'Buy Groceries', frequency: 14 }
+  { description: 'Wash Dishes', frequency: 1, active: true, dateLastPerformed: '7/10/2023' },
+  { description: 'Scoop Litterbox', frequency: 2, dateLastPerformed: '7/9/2023' },
+  { description: 'Vacuum Car', frequency: SIX_MONTHS, dateLastPerformed: '3/1/2023' },
+  { description: 'Vacuum Carpet', frequency: ONE_WEEK, dateLastPerformed: '7/8/2023' },
+  { description: 'Sweep Floors', frequency: ONE_WEEK, dateLastPerformed: '7/5/2023' },
+  { description: 'Water Plants', frequency: 3, dateLastPerformed: '7/10/2023' },
+  { description: 'Clip Cat Claws', frequency: TWO_WEEKS, dateLastPerformed: '7/8/2023' },
+  { description: 'Give Stormy Inhaler', frequency: 1, dateLastPerformed: '7/10/2023' },
+  { description: 'Buy Groceries', frequency: TWO_WEEKS, dateLastPerformed: '' }
 ];
